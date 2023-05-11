4d8e3c54dd7145de294f63de4fa6bc9611247674
Llop Dieguez Luca Santino <l.llop@etec.local>
2023-05-11 10:01:13 -0300

    crud python flask mysql

--- Prueba de concepto Flask Mongo/crud.py
+++
@@ -1,9 +0,0 @@
-
-def obtener_bd():
-    host = "localhost"
-    puerto = "27017"
-    usuario = "parzibyte"
-    palabra_secreta = "hunter2"
-    base_de_datos = "tienda"
-    cliente = MongoClient("mongodb://{}:{}@{}:{}".format(usuario, palabra_secreta, host, puerto))
-    return cliente[base_de_datos]
